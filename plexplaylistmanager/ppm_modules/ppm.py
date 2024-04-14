8620850b0774fc7fc61e5dcc00892478574d0cbd
0xChloe <chloe@catastrophesoftware.com>
2024-04-14 19:46:35 +0000

    Changed the way we process users

--- plexplaylistmanager/ppm_modules/ppm.py
+++ plexplaylistmanager/ppm_modules/ppm.py
@@ -1,13 +1,14 @@
 from radarr import RadarrAPI
 from plex import PlexService
 from sonarr import SonarrAPI
+from typing import List
 
-def radarr_tag_sync():
+def radarr_tag_sync(users: List):
     plex = PlexService()
     movies = []
     radarr = RadarrAPI()
     movie_dict = radarr.get_radarr_tags()
-    plex_users = ["alyssa"]
+    plex_users = users
     for key in movie_dict:
         if key in plex_users:
             for movie_id in movie_dict[key]:
@@ -16,18 +17,17 @@ def radarr_tag_sync():
                 movies.append(movie_title)
             plex.create_requests_list("Movies", movies, key)
 
-radarr_tag_sync()
 
-def sonarr_tag_sync():
+
+def sonarr_tag_sync(users: List):
     plex = PlexService()
     shows = []
     sonarr = SonarrAPI()
     show_dict = sonarr.get_sonarr_tags()
-    plex_users = ["alyssa"]  # Specify the list of Plex users you want to sync for
+    plex_users = users
     for key in show_dict:
         if key in plex_users:
             for show_id in show_dict[key]:
                 show_title = sonarr.get_show_titles(show_id)
                 shows.append(show_title)
             plex.create_requests_list("TV Shows", shows, key)
-
